58d699039b7d6762f8c8b97d6b768bb32294ebc3
Remy Menard <remymenard.work@gmail.com>
2020-08-13 16:51:30 +0200

    working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,22 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(get_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+  def get_params
+    params.require(:cocktail).permit(:name)
+  end
 end
